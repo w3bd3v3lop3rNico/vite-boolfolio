36beacd490de42f43974adb1d59c201fec1e7f2e
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-15 15:36:29 +0100

    Fix App root page including index page and sass

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script>
-
+    import ProjectIndex from './pages/Index.vue';
+    export default {
+        components: {
+            ProjectIndex,
+        }
+    }
 </script>
 
 <template>
+    <ProjectIndex />
   
 </template>
 
 <style lang="scss">
+@use "./style/general.scss" as *;
 
 </style>
