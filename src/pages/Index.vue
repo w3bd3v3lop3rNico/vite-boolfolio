299a400ebef97798bccb1683a7a84a8384df113b
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-15 15:49:22 +0100

    Fix Index.vue page with axios call

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,4 +1,26 @@
 <script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: [],
+      url: 'http://127.0.0.1:8000/api'
+    }
+  },
+  methods: {
+    fetchProjData() {
+      axios.get(`${this.url}/projects`)
+      .then((res) => {
+        console.log(res)
+        // this.projects = res.data.results.data
+      })
+    }
+  },
+  created() {
+    this. fetchProjData()
+  }
+}
 </script>
 <template>
 
