8d57e67a2c2208bf9ec605b7654d2fdab3d2843f
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-16 19:21:11 +0100

    Add axios call Add content

--- src/pages/projects/Show.vue
+++ src/pages/projects/Show.vue
@@ -10,11 +10,40 @@ export default {
       url: 'http://127.0.0.1:8000/api'
     }
   },
+  methods: {
+    fetchProject() {
+      // axios.get(`${this.url}/projects/${this.$route.params.slug}`)
+      axios.get(`${this.url}/projects/${this.slug}`)
+      .then(res => {
+        // console.log(res.data.project)
+        this.project = res.data.project
+      })
 
+        
+      
+    }
+  },
+  created() {
+    this.fetchProject()
+  }
 }
 </script>
 <template>
-   
+   <div v-if="project">
+    <div class="container">
+      <h1>{{  project.title }}</h1>
+      <p>{{ project.slug }}</p>
+      <!-- <p >{{ project.category?.name }}</p> -->
+      <ul class="technologies">
+        <li v-for="technology in project.technologies" :key="technology.id">
+          {{ technology.name }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="container" v-html="project.content">
+    </div>
+  </div>
 </template>
-<style>
+<style lang="scss" scoped>
 </style>
