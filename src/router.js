e523d40650f0a5ec7573756c12f039bcad61b3d7
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-16 17:07:58 +0100

    Add router js file fix main js with router

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Homepage from './pages/Homepage.vue';
+import ProjectIndex from './pages/projects/Index.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Homepage,
+        },
+        {
+			path: '/blog',
+			name: 'projects.index',
+			component: ProjectIndex,
+		},
+    ]
+})
+
+export { router }
