13f46ff68d95896719d80887dbae58f463822dec
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-16 09:50:37 +0100

    Add card project card Fix index file Add props

---
+++ src/components/ProjCard.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        props: {
+            project: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+<template>
+    <div class="card-content">
+        <h4>{{ project.title }}</h4>
+    </div>
+
+</template>
+<style lang="scss" scoped>
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,35 @@
 <script>
 import axios from 'axios';
+import ProjCard from '../components/ProjCard.vue'
 
 export default {
-  data() {
-    return {
-      projects: [],
-      url: 'http://127.0.0.1:8000/api'
+    components: {
+        ProjCard,
+    },
+    data() {
+        return {
+            projects: [],
+            url: 'http://127.0.0.1:8000/api'
+        }
+    },
+    methods: {
+        fetchProjData() {
+            axios.get(`${this.url}/projects`)
+                .then((res) => {
+                    console.log(res)
+                    this.projects = res.data.results
+                })
+        }
+    },
+    created() {
+        this.fetchProjData()
     }
-  },
-  methods: {
-    fetchProjData() {
-      axios.get(`${this.url}/projects`)
-      .then((res) => {
-        console.log(res)
-        // this.projects = res.data.results.data
-      })
-    }
-  },
-  created() {
-    this. fetchProjData()
-  }
 }
 </script>
 <template>
-
+    <div class="container">
+        <ProjCard class="card prject-card" v-for="project in projects" :project="project" :key="project.id" />
+    </div>
 </template>
 <style>
 </style>
